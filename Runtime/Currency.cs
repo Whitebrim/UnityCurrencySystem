20e525172698d0239a7b53843f00caec61258dc6
Денисов Ярослав <deni7off@yandex.ru>
2022-04-19 01:11:57 +0300

    Initial Commit

---
+++ Runtime/Currency.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CurrencySystem
+{
+    public class Currency
+    {
+        public readonly string name;
+        private double amount;
+    }
+}
---
+++ Runtime/Wallet.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CurrencySystem
+{
+    public class Wallet
+    {
+        private Dictionary<string, Currency> _wallet;
+    }
+}
